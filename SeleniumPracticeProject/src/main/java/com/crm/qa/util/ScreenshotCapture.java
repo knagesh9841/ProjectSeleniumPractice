9894a23712433cc1c11f5d5b207e911c25ac475d
Nagesh Mahadev Kadam <knagesh9841@yahoo.com>
2019-02-15 23:41:32 +0530

    Practice 15 feb

--- SeleniumPracticeProject/src/main/java/com/crm/qa/util/ScreenshotCapture.java
+++ SeleniumPracticeProject/src/main/java/com/crm/qa/util/ScreenshotCapture.java
@@ -1,13 +1,23 @@
 package com.crm.qa.util;
 
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.screentaker.ViewportPastingStrategy;
 
 public class ScreenshotCapture {
 	
@@ -21,14 +31,33 @@ public class ScreenshotCapture {
 	
 	synchronized public static String getScreenshot(WebDriver driver, String screenshotName) throws Exception {
 		
+		/*
+		  String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		  TakesScreenshot ts = (TakesScreenshot) driver; File source =
+		  ts.getScreenshotAs(OutputType.FILE);
+		  
+		  String destination = System.getProperty("user.dir") +
+		  "\\test-output\\"+screenshotName+dateName+".png"; File finalDestination = new
+		  File(destination); FileUtils.copyFile(source, finalDestination);
+		 
+		*/
+		
+		
 		String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
-		TakesScreenshot ts = (TakesScreenshot) driver;
-		File source = ts.getScreenshotAs(OutputType.FILE);
-		
-		String destination = System.getProperty("user.dir") + "\\test-output\\"+screenshotName+dateName+".png";
+		BufferedImage image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+		String destination = System.getProperty("user.dir") + "\\test-output\\"+screenshotName+dateName+".png"; 
 		File finalDestination = new File(destination);
-		FileUtils.copyFile(source, finalDestination);
+		ImageIO.write(image, "png", finalDestination); 
 		
+		
+		
+		/*
+		String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		Screenshot screenshot = new AShot().shootingStrategy(new ViewportPastingStrategy(1000)).takeScreenshot(driver);
+		String destination = System.getProperty("user.dir") + "\\test-output\\"+screenshotName+dateName+".png"; 
+		File finalDestination = new File(destination);
+		ImageIO.write(screenshot.getImage(), "PNG",finalDestination);
+		*/
 		
 		return destination;
 		
