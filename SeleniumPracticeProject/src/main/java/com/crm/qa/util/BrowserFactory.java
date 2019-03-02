7825a56f26999c1352b4e07dc4503df6e34fbb99
Nagesh Mahadev Kadam <knagesh9841@yahoo.com>
2019-03-02 14:40:38 +0530

    Selenium grid Practice 02 March

--- SeleniumPracticeProject/src/main/java/com/crm/qa/util/BrowserFactory.java
+++ SeleniumPracticeProject/src/main/java/com/crm/qa/util/BrowserFactory.java
@@ -1,11 +1,15 @@
 package com.crm.qa.util;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -32,6 +36,16 @@ public class BrowserFactory {
 			{
 				WebDriverManager.firefoxdriver().setup();
 				tlDriver = ThreadLocal.withInitial(() -> new FirefoxDriver(OptionsManager.getFirefoxOptions()));
+				
+				/*//For Grid Usage
+				try { 
+					tlDriver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), OptionsManager.getFirefoxOptions())); }
+				 catch (MalformedURLException e) 
+				 { 
+				 	e.printStackTrace(); 
+				 }
+				 */
+				
 				Log.info("-----------"+browsername+" Browser is initialized successfully.-------------");
 				 
 			}
@@ -39,6 +53,14 @@ public class BrowserFactory {
 			{
 				 WebDriverManager.chromedriver().setup();
 				 tlDriver = ThreadLocal.withInitial(() -> new ChromeDriver(OptionsManager.getChromeOptions()));
+				 
+				  /*//For Grid Usage
+		            try {
+		                tlDriver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), OptionsManager.getChromeOptions()));
+		            } catch (MalformedURLException e) {
+		                e.printStackTrace();
+		            }*/
+				 
 				 Log.info("-----------"+browsername+" Browser is initialized successfully.-------------");
 				 
 			}
@@ -47,12 +69,28 @@ public class BrowserFactory {
 				//WebDriverManager.iedriver().setup();
 				WebDriverManager.iedriver().architecture(io.github.bonigarcia.wdm.Architecture.X32).setup();
 				tlDriver = ThreadLocal.withInitial(() -> new InternetExplorerDriver(OptionsManager.getIEOptions()));
+				
+				 /*//For Grid Usage
+	            try {
+	                tlDriver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), OptionsManager.getIEOptions()));
+	            } catch (MalformedURLException e) {
+	                e.printStackTrace();
+	            }*/
+				
 				Log.info("-----------"+browsername+" Browser is initialized successfully.-------------");
 			}
 			else if(browsername.equalsIgnoreCase("edge"))
 			{
 				WebDriverManager.edgedriver().setup();
 				tlDriver = ThreadLocal.withInitial(() -> new EdgeDriver(OptionsManager.getEdgeOptions()));
+				
+				 /*//For Grid Usage
+	            try {
+	                tlDriver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), OptionsManager.getEdgeOptions()));
+	            } catch (MalformedURLException e) {
+	                e.printStackTrace();
+	            }*/
+				
 				Log.info("-----------"+browsername+" Browser is initialized successfully.-------------");
 				
 			}else
